57b8933e51ad1dcc6f46dfa2741524d874901e83
Prachi Ramesh Godase <prachigodase2@gmail.com>
2023-11-20 19:54:00 +0530

    adding logout button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthForm from './components/AuthForm';
 import ChatUi from './components/ChatUi';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<AuthForm />} />
         <Route path="/chat" element={<ChatUi />} />
+       
       
       </Routes>
     </Router>
---
+++ src/components/Navbar.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState} from "react";
+import { useSelector, useDispatch } from "react-redux";
+import classes from "../components/Navbar.module.css"
+import { Link } from "react-router-dom";
+import { authActions } from "../store/AuthReducer"
+import { useNavigate } from "react-router-dom";
+import axios from "axios"
+import { Avatar,Tooltip } from '@mui/material';
+function Navbar() {
+    const [isHovered, setIsHovered] = useState(false);
+    const dispatch = useDispatch();
+    const isAuth = useSelector((state) => state.auth.isAuthenticated);
+    const token=localStorage.getItem('token')
+    const enteredEmail=localStorage.getItem('email')
+    const navigate = useNavigate();
+
+
+    const toggleAvtar=()=>{
+        setIsHovered((prevValue) => !prevValue);
+      }
+
+      useEffect(()=>{
+        dispatch(authActions.islogin(token))
+      })
+
+      const logOutHandler=()=>{
+        dispatch(authActions.islogout())
+        navigate("/")
+      }
+    
+  return (
+    <div>
+      <nav className="p-3  bg-gradient-to-b from-blue-900 to-purple-400 items-center">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <h1 className={`mr-8 text-gray-100 font-bold ${classes.logo} `}>
+          Group Chat App
+        </h1>
+
+        <div className="flex">
+          {!isAuth && (
+            <Link to="/">
+              <button className='bg-gradient-to-b from-red-600 via-red-500 to-red-800  hover:bg-purple-600 py-2 px-4 font-bold text-white rounded'>LOGIN</button>
+            </Link>
+          )}
+          {isAuth && (
+              <Tooltip
+                title={enteredEmail}
+                placement="bottom"
+                open={isHovered}
+                onClose={() => setIsHovered(false)}
+                onOpen={() => setIsHovered(true)}
+              >
+                <Avatar
+                className="bg-gradient-to-b from-yellow-200 to-pink-600 mr-3"
+              style={{ marginRight: "10px", cursor: "pointer"}}
+                  onMouseEnter={() => setIsHovered(true)}
+                  onMouseLeave={() => setIsHovered(false)}
+                  onClick={toggleAvtar}
+                >
+                  {!isHovered ? (
+                    <span className={classes.avatarText}>
+                      {enteredEmail && <Avatar className="bg-gradient-to-b from-yellow-200 to-pink-700 " />}
+                    </span>
+                  ) : (
+                    <span>
+                      {enteredEmail && enteredEmail.charAt(0).toUpperCase()}
+                    </span>
+                  )}
+                </Avatar>
+              </Tooltip>
+            )}
+         
+          
+          {isAuth && (
+            <button
+              className='bg-gradient-to-b from-red-600 via-red-500 to-red-800  hover:bg-purple-600 py-2 px-4 font-bold text-white rounded mx-5 '
+              onClick={logOutHandler}
+            >
+              LOGOUT
+            </button>
+          )}
+        </div>
+      </div>
+    </nav>
+    </div>
+  )
+}
+
+export default Navbar
